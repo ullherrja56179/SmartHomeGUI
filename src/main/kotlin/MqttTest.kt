63fa29f9ce50fc8433d1e3192984173f9a9a2539
Jakob Ullherr <jakob.ullherr@freenet.de>
2021-11-06 14:46:33 +0100

    MqttTest.kt: Add command to remove device from knownDevices. Add output when done with executing the command. add exit command.

--- src/main/kotlin/MqttTest.kt
+++ src/main/kotlin/MqttTest.kt
@@ -3,22 +3,31 @@ import util.Util
 fun main() {
     val listener: DeviceListener = DeviceListener()
 
-    while (true)
-    {
+    while (true) {
         val command = readLine()?.split(" ")
-        when (command?.get(0)?.lowercase())
-        {
-            "addnew" -> listener.startListeningForNewDevices()
-            "getdevices" -> println(Util.knownDevices)
+        when (command?.get(0)?.lowercase()) {
+            "addnew" -> {
+                listener.startListeningForNewDevices()
+                println("----- DONE -----")
+            }
+            "getdevices" -> {
+                println(Util.knownDevices)
+                println("----- DONE -----")
+
+            }
             "set" -> {
                 val deviceName = command[1]
                 val key = command[2]
                 val value = command[3]
                 Util.knownDevices[deviceName]?.handleSet(key, value) ?: continue
+                println("----- DONE -----")
             }
-            else -> break
+            "remove" -> {
+                Util.knownDevices.remove(command[1])
+                println("----- DONE -----")
+            }
+            "exit" -> break
+            else -> println("Unknown Command")
         }
     }
-
-
 }
