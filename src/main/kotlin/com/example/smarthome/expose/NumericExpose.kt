0343430127b995bcfdf789e67e2031709ae8d839
Jakob Ullherr <jakob.ullherr@freenet.de>
2022-05-30 18:23:11 +0200

    NumericExpose.kt: Add value_min and value_max

--- src/main/kotlin/com/example/smarthome/expose/NumericExpose.kt
+++ src/main/kotlin/com/example/smarthome/expose/NumericExpose.kt
@@ -5,11 +5,12 @@ import java.lang.NumberFormatException
 data class NumericExpose(override val name: String,
                          override val description: String,
                          override val presets: List<String>,
-                         private val value_min: Int,
-                         private val value_max: Int,
+                         val value_min: Int,
+                         val value_max: Int,
                          override val isSet: Boolean,
                          override val isGet: Boolean ) : ExposeObject
 {
+    var currentValue: String = ((value_min+value_max)/2).toString();
     override fun validateValue(value: Any): Boolean {
         return if (value is String && value.lowercase() in presets) true
         else {
